72920601e865a582a9312a66e1fe2be10e33c4f3
Grjii <georgeejbbr@gmail.com>
2024-10-21 10:35:53 +0400

    Add files via upload

---
+++ Ex-2.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+"""
+Chapter-1 Exercise 2
+
+"""
+
+word_1 = '                 Python'
+new_word_1 = word_1.lstrip()
+print(word_1)
+print(new_word_1)
+
+# use of rstrip()
+
+word_2 = 'Python                     is cool'
+new_word_2 = word_2.rstrip()
+print(word_2)
+print(new_word_2)
+
+# use of strip()
+
+word_3 = '               Nice work             '
+new_word_3 = word_3.strip()
+print(word_3)
+print(new_word_3)
+
+# use of \n
+
+print('Friends I made during the first month of the semester: \n Asma \n Joe \n Obaid \n Faiza')
+
+# use of \t
+
+print('The tallest building in the world is \t Burj Khalifa')
